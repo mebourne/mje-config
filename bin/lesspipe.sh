e8af554c25cfa9893c2a7d7d437e3d76dfa097cb
Martin Ebourne <martin@zepler.org>
2000-11-02 11:11:12 +0000

    Improved check for man pages so it's less likely to bodge a normal file.

--- bin/lesspipe.sh
+++ bin/lesspipe.sh
@@ -8,6 +8,7 @@ case "$1" in
      ;;
 *.gz) gunzip -c $1 2>/dev/null
       ;;
-*.[0-9]) nroff -man $1 2>/dev/null
+*.[0-9]) # Check for a line near the top starting with a '.' command
+	 head -n 3 $1 | grep -q '^\.' && nroff -man $1 2>/dev/null
          ;;
 esac
