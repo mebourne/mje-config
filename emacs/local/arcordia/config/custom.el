b3f3b118274c0e018531cd1035b1a382c6cd440d
Martin Ebourne <martin@zepler.org>
2001-05-16 17:13:57 +0000

    Initial revision

---
+++ emacs/local/arcordia/config/custom.el
@@ -0,0 +1,13 @@
+;; Emacs configuration file
+;; Customised variables
+;; Written by Martin Ebourne
+;; $Id: custom.el,v 1.1 2001/05/16 17:13:57 mebourne Exp $
+
+;; Change local customisation defaults
+(custom-set-variables
+
+ ;; Set physical location
+ '(calendar-longitude -0.08)
+ '(calendar-latitude 51.52)
+ '(calendar-location-name "City, London")
+ )
