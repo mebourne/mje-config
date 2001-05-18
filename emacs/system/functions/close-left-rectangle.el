1bf361842a28a80ee274f6df79694bc73a348cab
Martin Ebourne <martin@zepler.org>
2001-05-18 13:19:01 +0000

    Initial revision

---
+++ emacs/system/functions/close-left-rectangle.el
@@ -0,0 +1,21 @@
+;; Emacs function file
+;; Close rectangle to the left, opposite of close-rectangle
+;; Written by Martin Ebourne
+;; $Id: close-left-rectangle.el,v 1.1 2001/05/18 13:19:01 mebourne Exp $
+
+;;;###autoload
+(defun close-left-rectangle (start end)
+  "Delete all whitespace preceeding a specified column in each line.
+The left edge of the rectangle specifies the position in each line
+at which whitespace deletion should end.  On each line in the
+rectangle, all continuous whitespace finishing at that column is
+deleted."
+  (interactive "r")
+  (operate-on-rectangle '(lambda (startpos begextra endextra)
+			   (save-excursion
+			     (goto-char startpos)
+			     (delete-region (point)
+					    (progn
+					      (skip-syntax-backward " ")
+					      (point)))))
+			start end t))
