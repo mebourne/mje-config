b4bbfd1c9ad52dd0082e45f0973d6975906155a6
Martin Ebourne <martin@zepler.org>
2002-03-21 10:30:36 +0000

    Moved company styles to local directories.

--- emacs/system/modes/cc-mode.el
+++ emacs/system/modes/cc-mode.el
@@ -1,7 +1,7 @@
 ;; Emacs configuration file
 ;; Setup for cc-mode
 ;; Written by Martin Ebourne
-;; $Id: cc-mode.el,v 1.5 2002/03/21 10:03:21 mebourne Exp $
+;; $Id: cc-mode.el,v 1.6 2002/03/21 10:30:36 mebourne Exp $
 
 ;; C and C++ style
 (eval-after-load "cc-mode"
@@ -112,67 +112,10 @@
 	       ;;(c-cleanup-list . (empty-defun-braces))
 	       (c-electric-pound-behavior . 'alignleft)
 	       ))
-(c-add-style "gs-java"
-	     '("java"
-	       (c-basic-offset . 4)
-	       (c-offsets-alist . ((statement-cont    . c-lineup-math)
-				   (substatement-open . 0)
-				   (statement-case-open . 0)
-				   (case-label        . +)
-				   ))
-	       (c-hanging-braces-alist . ((defun-open before)
-					  (defun-close before)
-					  (class-open before)
-					  (class-close before)
-					  (inline-open before)
-					  (inline-close before)
-					  (block-open before)
-					  (block-close before)
-					  (substatement-open before)
-					  (statement-case-open before)
-					  (extern-lang-open before)
-					  (extern-lang-close before)
-					  (brace-list-open)
-					  (brace-list-close before)
-					  (brace-list-intro)
-					  (brace-list-entry)
-					  ))
-	       (c-hanging-semi&comma-criteria . nil)
-	       ;;(c-cleanup-list . (empty-defun-braces))
-	       (c-electric-pound-behavior . 'alignleft)
-	       ))
-(c-add-style "iqfs"
-	     '((c-basic-offset . 4)
-	       (c-offsets-alist . ((statement-cont    . c-lineup-math)
-				   (substatement-open . 0)
-				   (case-label        . +)
-				   (access-label      . -)
-				   ))
-	       (c-hanging-braces-alist . ((defun-open before)
-					  (defun-close before)
-					  (class-open before)
-					  (class-close before)
-					  (inline-open)
-					  (inline-close)
-					  (block-open before)
-					  (block-close before)
-					  (substatement-open before)
-					  (statement-case-open before)
-					  (extern-lang-open before)
-					  (extern-lang-close before)
-					  (brace-list-open)
-					  (brace-list-close before)
-					  (brace-list-intro)
-					  (brace-list-entry)
-					  ))
-	       (c-hanging-semi&comma-criteria . nil)
-	       ;;(c-cleanup-list . (empty-defun-braces))
-	       (c-electric-pound-behavior . 'alignleft)
-	       ))
 
 ;; Set up our C style
 (custom-set-variables
- '(c-default-style '((java-mode . "gs-java")
+ '(c-default-style '((java-mode . "martin-java")
 		     (other     . "martin")
 		     ))
  )
