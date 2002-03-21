130d193cfd85c789dd0be5a199ee4b0712482cff
Martin Ebourne <martin@zepler.org>
2002-03-21 13:40:07 +0000

    Style now derives from martin. Eliminate trailing spaces. .h files are C++.

--- emacs/local/csfb/modes/cc-mode.el
+++ emacs/local/csfb/modes/cc-mode.el
@@ -1,36 +1,14 @@
 ;; Emacs configuration file
 ;; Setup for cc-mode
 ;; Written by Martin Ebourne
-;; $Id: cc-mode.el,v 1.2 2002/03/21 12:22:57 mebourne Exp $
+;; $Id: cc-mode.el,v 1.3 2002/03/21 13:40:07 mebourne Exp $
 
 ;; Add our styles
 (c-add-style "csfb"
-	     '((c-basic-offset . 3)
-	       (c-offsets-alist . ((statement-cont    . c-lineup-math)
-				   (substatement-open . 0)
-				   (case-label        . +)
-				   (access-label      . -)
+	     '("martin"
+	       (c-basic-offset . 3)
+	       (c-offsets-alist . ((access-label      . -)
 				   ))
-	       (c-hanging-braces-alist . ((defun-open before)
-					  (defun-close before)
-					  (class-open before)
-					  (class-close before)
-					  (inline-open)
-					  (inline-close)
-					  (block-open before)
-					  (block-close before)
-					  (substatement-open before)
-					  (statement-case-open before)
-					  (extern-lang-open before)
-					  (extern-lang-close before)
-					  (brace-list-open)
-					  (brace-list-close before)
-					  (brace-list-intro)
-					  (brace-list-entry)
-					  ))
-	       (c-hanging-semi&comma-criteria . nil)
-	       ;;(c-cleanup-list . (empty-defun-braces))
-	       (c-electric-pound-behavior . 'alignleft)
 	       (indent-tabs-mode . nil)
 	       ))
 
@@ -40,3 +18,14 @@
 		     (other     . "csfb")
 		     ))
  )
+
+;; Strip trailing spaces on save
+(add-hook 'c-mode-common-hook
+	  (function (lambda ()
+		      (add-hook 'write-contents-hooks 'eliminate-trailing-spaces)
+		      )))
+
+;; .h is C++
+(setq auto-mode-alist
+      (append '(("\\.h\\'" . c++-mode)
+		) auto-mode-alist))
