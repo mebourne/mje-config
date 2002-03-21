475f2026001e2fccc7590bf79034012f138117a0
Martin Ebourne <martin@zepler.org>
2002-03-21 13:40:45 +0000

    Style now derives from martin. Eliminate trailing spaces.

--- emacs/local/gs/modes/cc-mode.el
+++ emacs/local/gs/modes/cc-mode.el
@@ -1,37 +1,12 @@
 ;; Emacs configuration file
 ;; Setup for cc-mode
 ;; Written by Martin Ebourne
-;; $Id: cc-mode.el,v 1.1 2002/03/21 10:29:57 mebourne Exp $
+;; $Id: cc-mode.el,v 1.2 2002/03/21 13:40:45 mebourne Exp $
 
 ;; Add our styles
 (c-add-style "gs-java"
-	     '("java"
+	     '("martin-java"
 	       (c-basic-offset . 4)
-	       (c-offsets-alist . ((statement-cont    . c-lineup-math)
-				   (substatement-open . 0)
-				   (statement-case-open . 0)
-				   (case-label        . +)
-				   ))
-	       (c-hanging-braces-alist . ((defun-open before)
-					  (defun-close before)
-					  (class-open before)
-					  (class-close before)
-					  (inline-open before)
-					  (inline-close before)
-					  (block-open before)
-					  (block-close before)
-					  (substatement-open before)
-					  (statement-case-open before)
-					  (extern-lang-open before)
-					  (extern-lang-close before)
-					  (brace-list-open)
-					  (brace-list-close before)
-					  (brace-list-intro)
-					  (brace-list-entry)
-					  ))
-	       (c-hanging-semi&comma-criteria . nil)
-	       ;;(c-cleanup-list . (empty-defun-braces))
-	       (c-electric-pound-behavior . 'alignleft)
 	       ))
 
 ;; Set up our C style
@@ -40,3 +15,9 @@
 		     (other     . "martin")
 		     ))
  )
+
+;; Strip trailing spaces on save
+(add-hook 'c-mode-common-hook
+	  (function (lambda ()
+		      (add-hook 'write-contents-hooks 'eliminate-trailing-spaces)
+		      )))
