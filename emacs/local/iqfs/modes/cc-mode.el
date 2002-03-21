93d7d92219017da19bd9a220fc5eee3e474dc0b6
Martin Ebourne <martin@zepler.org>
2002-03-21 13:40:55 +0000

    Style now derives from martin.

--- emacs/local/iqfs/modes/cc-mode.el
+++ emacs/local/iqfs/modes/cc-mode.el
@@ -1,36 +1,17 @@
 ;; Emacs configuration file
 ;; Setup for cc-mode
 ;; Written by Martin Ebourne
-;; $Id: cc-mode.el,v 1.2 2002/03/21 13:23:09 mebourne Exp $
+;; $Id: cc-mode.el,v 1.3 2002/03/21 13:40:55 mebourne Exp $
 
 ;; Add our styles
 (c-add-style "iqfs"
-	     '((c-basic-offset . 4)
+	     '("martin"
+	       (c-basic-offset . 4)
 	       (c-offsets-alist . ((statement-cont    . c-lineup-math)
 				   (substatement-open . 0)
 				   (case-label        . +)
 				   (access-label      . -)
 				   ))
-	       (c-hanging-braces-alist . ((defun-open before)
-					  (defun-close before)
-					  (class-open before)
-					  (class-close before)
-					  (inline-open)
-					  (inline-close)
-					  (block-open before)
-					  (block-close before)
-					  (substatement-open before)
-					  (statement-case-open before)
-					  (extern-lang-open before)
-					  (extern-lang-close before)
-					  (brace-list-open)
-					  (brace-list-close before)
-					  (brace-list-intro)
-					  (brace-list-entry)
-					  ))
-	       (c-hanging-semi&comma-criteria . nil)
-	       ;;(c-cleanup-list . (empty-defun-braces))
-	       (c-electric-pound-behavior . 'alignleft)
 	       (tab-width . 4)
 	       ))
 
