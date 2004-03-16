ea1b65fde14dc2fc78dd7e5935cc1b4a4eb91140
Martin Ebourne <martin@zepler.org>
2004-03-16 15:21:54 +0000

    Added postgresql support. Also fixed use of autocommit on sybase.

--- perl/programs/sqlExecute.pl
+++ perl/programs/sqlExecute.pl
@@ -2,11 +2,11 @@
 #
 # Format SQL output to make it easier to read
 # Written by Martin Ebourne. Started 22/05/01
-# $Id: sqlExecute.pl 792 2003-09-22 11:47:18Z martin $
+# $Id$
 
 use strict;
 use Data::Dumper;
-use DBI;
+use DBI ();
 use English;
 use Env;
 use MJE::ParseOpts;
@@ -15,36 +15,6 @@ use MJE::TableFormatter;
 BEGIN {
 #  $ENV{ORACLE_HOME}="/app/oracle/product/client/8.1.6";
 }
-
-# Map from database types to TableFormatter types
-my %types=(DBI::SQL_CHAR		, "string",
-	   DBI::SQL_LONGVARCHAR		, "string",
-	   DBI::SQL_VARCHAR		, "string",
-	   DBI::SQL_WCHAR		, "string",
-	   DBI::SQL_WLONGVARCHAR	, "string",
-	   DBI::SQL_WVARCHAR		, "string",
-	   -9112                        , "string",  # Oracle CLOB
-
-#	   DBI::SQL_BIGINT		, "number",
-	   DBI::SQL_DECIMAL		, "number",
-	   DBI::SQL_DOUBLE		, "number",
-	   DBI::SQL_FLOAT		, "number",
-	   DBI::SQL_INTEGER		, "number",
-	   DBI::SQL_NUMERIC		, "number",
-	   DBI::SQL_REAL		, "number",
-	   DBI::SQL_SMALLINT		, "number",
-	   DBI::SQL_TINYINT		, "number",
-
-	   DBI::SQL_DATE		, "datetime",
-	   DBI::SQL_TIME		, "datetime",
-	   DBI::SQL_TIMESTAMP		, "datetime",
-
-	   DBI::SQL_BINARY		, "binary",
-	   DBI::SQL_BIT			, "binary",
-	   DBI::SQL_LONGVARBINARY	, "binary",
-	   DBI::SQL_VARBINARY		, "binary",
-	   -9113                        , "binary",  # Oracle BLOB
-	   );
 
 # Generate a simple hash from style name to description for use with
 # ParseOpts. Also generate summary for main help page
@@ -75,6 +45,9 @@ Usage:
 sqlExecute.pl [options] <sql-statement> ...
 
 Options:
+  -a, --auto-commit		Enable auto-commit (default is single
+				transaction)
+				# --auto-commit | -a
   -c <when>, --colour=<when>	Colour output. One of:
 				# --colour | -c : values=%colourHelp
 				off	No colour
@@ -82,7 +55,7 @@ Options:
 				on	Always colour (if applicable)
   -C <connect>, --connect=<connect>
 				The DBI connect string to use (see individual
-	 	 	 	driver documentation)
+				driver documentation)
 				# --connect | -C : string
   --column-width=<count>	Specify the maximum column width to be shown
 				# --column-width : posinteger
@@ -112,7 +85,7 @@ Arguments:
   <sql-statement> ...		SQL statement(s) to execute. If a statement is
 				in parentheses then the row count will not be
 				reported for it
-	   			# @statements += [*] sql : string
+				# @statements += [*] sql : string
 ') || exit 1;
 
 # Note if we are outputting to a tty or a file
@@ -137,6 +110,114 @@ if(!exists($opts->{driver}) || !exists($opts->{user})) {
   die "Must specify database driver and user";
 }
 
+require "DBD/$opts->{driver}.pm";
+
+# Map from database types to TableFormatter types
+no strict qw(subs);
+my %types=(DBI::SQL_CHAR                          , "string",
+           DBI::SQL_CLOB                          , "string",
+           DBI::SQL_LONGVARCHAR                   , "string",
+           DBI::SQL_VARCHAR                       , "string",
+           DBI::SQL_WCHAR                         , "string",
+           DBI::SQL_WLONGVARCHAR                  , "string",
+           DBI::SQL_WVARCHAR                      , "string",
+           DBD::Oracle::ORA_CHAR                  , "string",
+           DBD::Oracle::ORA_CHARZ                 , "string",
+           DBD::Oracle::ORA_CLOB                  , "string",
+           DBD::Oracle::ORA_LONG                  , "string",
+           DBD::Oracle::ORA_STRING                , "string",
+           DBD::Oracle::ORA_VARCHAR2              , "string",
+           DBD::Pg::PG_BPCHAR                     , "string",
+           DBD::Pg::PG_CHAR                       , "string",
+           DBD::Pg::PG_TEXT                       , "string",
+           DBD::Pg::PG_VARCHAR                    , "string",
+
+           DBI::SQL_BOOLEAN                       , "number",
+           DBI::SQL_DECIMAL                       , "number",
+           DBI::SQL_DOUBLE                        , "number",
+           DBI::SQL_FLOAT                         , "number",
+           DBI::SQL_INTEGER                       , "number",
+           DBI::SQL_NUMERIC                       , "number",
+           DBI::SQL_REAL                          , "number",
+           DBI::SQL_SMALLINT                      , "number",
+           DBI::SQL_TINYINT                       , "number",
+           DBD::Oracle::ORA_NUMBER                , "number",
+           DBD::Oracle::ORA_ROWID                 , "number",
+           DBD::Pg::PG_BOOL                       , "number",
+           DBD::Pg::PG_FLOAT4                     , "number",
+           DBD::Pg::PG_FLOAT8                     , "number",
+           DBD::Pg::PG_INT2                       , "number",
+           DBD::Pg::PG_INT4                       , "number",
+           DBD::Pg::PG_INT8                       , "number",
+           DBD::Pg::PG_OID                        , "number",
+
+           DBI::SQL_DATE                          , "datetime",
+           DBI::SQL_DATETIME                      , "datetime",
+           DBI::SQL_INTERVAL                      , "datetime",
+           DBI::SQL_INTERVAL_DAY                  , "datetime",
+           DBI::SQL_INTERVAL_DAY_TO_HOUR          , "datetime",
+           DBI::SQL_INTERVAL_DAY_TO_MINUTE        , "datetime",
+           DBI::SQL_INTERVAL_DAY_TO_SECOND        , "datetime",
+           DBI::SQL_INTERVAL_HOUR                 , "datetime",
+           DBI::SQL_INTERVAL_HOUR_TO_MINUTE       , "datetime",
+           DBI::SQL_INTERVAL_HOUR_TO_SECOND       , "datetime",
+           DBI::SQL_INTERVAL_MINUTE               , "datetime",
+           DBI::SQL_INTERVAL_MINUTE_TO_SECOND     , "datetime",
+           DBI::SQL_INTERVAL_MONTH                , "datetime",
+           DBI::SQL_INTERVAL_SECOND               , "datetime",
+           DBI::SQL_INTERVAL_YEAR                 , "datetime",
+           DBI::SQL_INTERVAL_YEAR_TO_MONTH        , "datetime",
+           DBI::SQL_TIME                          , "datetime",
+           DBI::SQL_TIMESTAMP                     , "datetime",
+           DBI::SQL_TYPE_DATE                     , "datetime",
+           DBI::SQL_TYPE_TIME                     , "datetime",
+           DBI::SQL_TYPE_TIMESTAMP                , "datetime",
+           DBI::SQL_TYPE_TIMESTAMP_WITH_TIMEZONE  , "datetime",
+           DBI::SQL_TYPE_TIME_WITH_TIMEZONE       , "datetime",
+           DBD::Oracle::ORA_DATE                  , "datetime",
+           DBD::Pg::PG_ABSTIME                    , "datetime",
+           DBD::Pg::PG_DATE                       , "datetime",
+           DBD::Pg::PG_DATETIME                   , "datetime",
+           DBD::Pg::PG_RELTIME                    , "datetime",
+           DBD::Pg::PG_TIME                       , "datetime",
+           DBD::Pg::PG_TIMESPAN                   , "datetime",
+           DBD::Pg::PG_TIMESTAMP                  , "datetime",
+           DBD::Pg::PG_TINTERVAL                  , "datetime",
+
+           DBI::SQL_BINARY                        , "binary",
+           DBI::SQL_BIT                           , "binary",
+           DBI::SQL_BLOB                          , "binary",
+           DBI::SQL_LONGVARBINARY                 , "binary",
+           DBI::SQL_VARBINARY                     , "binary",
+           DBD::Oracle::ORA_BLOB                  , "binary",
+           DBD::Oracle::ORA_LONGRAW               , "binary",
+           DBD::Oracle::ORA_RAW                   , "binary",
+           DBD::Pg::PG_BYTEA                      , "binary",
+
+           # What to do with these?
+           DBI::SQL_GUID                          , "unknown",
+           DBI::SQL_UNKNOWN_TYPE                  , "unknown",
+           DBI::SQL_UDT                           , "unknown",
+           DBI::SQL_UDT_LOCATOR                   , "unknown",
+           DBI::SQL_ROW                           , "unknown",
+           DBI::SQL_REF                           , "unknown",
+           DBI::SQL_BLOB_LOCATOR                  , "unknown",
+           DBI::SQL_CLOB_LOCATOR                  , "unknown",
+           DBI::SQL_ARRAY                         , "unknown",
+           DBI::SQL_ARRAY_LOCATOR                 , "unknown",
+           DBI::SQL_MULTISET                      , "unknown",
+           DBI::SQL_MULTISET_LOCATOR              , "unknown",
+           DBD::Oracle::ORA_MLSLABEL              , "unknown",
+           DBD::Oracle::ORA_NTY                   , "unknown",
+           DBD::Oracle::ORA_RSET                  , "unknown",
+           );
+use strict qw(subs);
+
+# Sybase helpfully returns a 'BIT' as an ASCII character
+if($opts->{driver} eq "Sybase") {
+  $types{DBI::SQL_BIT}="number";
+}
+
 &main();
 
 exit;
@@ -156,9 +237,11 @@ sub raiseError {
   }
 
   $errh->finish if $errh!=$dbh;
-  $dbh->rollback;
+  if(!exists($opts->{"auto-commit"})) {
+    $dbh->rollback;
+    print STDERR "Changes rolled back.\n";
+  }
   $dbh->disconnect;
-  print STDERR "Changes rolled back.\n";
 
   exit 1;
 }
@@ -181,16 +264,16 @@ sub main {
 
   # Connect to the database
   my $dbh = DBI->connect("dbi:$opts->{driver}:" . (exists($opts->{connect})
-						   ? $opts->{connect}
-						   : ""),
-			 $opts->{user},
-			 $opts->{password},
-			 { AutoCommit  => 0,
-			   ChopBlanks  => 1,
-			   LongReadLen => $dataLength,
-			   LongTruncOk => 1,
-			   PrintError  => 0,
-			   RaiseError  => 0 })
+                                                   ? $opts->{connect}
+                                                   : ""),
+                         $opts->{user},
+                         $opts->{password},
+                         { AutoCommit  => exists($opts->{"auto-commit"}),
+                           ChopBlanks  => 1,
+                           LongReadLen => $dataLength,
+                           LongTruncOk => 1,
+                           PrintError  => 0,
+                           RaiseError  => 0 })
       || die "Can't connect to $opts->{driver}: $DBI::errstr";
 
   # Iterate over all of the statements
@@ -204,59 +287,75 @@ sub main {
 
     # Prepare the statement
     my $sth = $dbh->prepare($statement)
-	|| &raiseError($dbh,$dbh,"dbh->prepare",$statement);
+        || &raiseError($dbh,$dbh,"dbh->prepare",$statement);
 
     # Execute the statement
     $sth->execute || &raiseError($dbh,$sth,"sth->execute",$statement);
 
     # Output results if a select statement
     if($sth->{NUM_OF_FIELDS}) {
+      do {
 
-      # List the columns for the formatter
-      for(my $i=0;$i<$sth->{NUM_OF_FIELDS};$i++) {
+        if(exists($sth->{syb_result_type})
+           && $sth->{syb_result_type}==eval("DBD::Sybase::CS_STATUS_RESULT")) {
+          my @result = $sth->fetchrow_array;
+          print "(return status = $result[0])\n";
+          $sth->fetchrow_array;
+        } else {
+          $formatter->reset;
 
-	if(!exists($types{$sth->{TYPE}->[$i]})) {
-	  print STDERR "Warning: Unknown database type $sth->{TYPE}->[$i]\n";
-	}
+          # List the columns for the formatter
+          for(my $i=0;$i<$sth->{NUM_OF_FIELDS};$i++) {
 
-	$formatter->addColumn($sth->{NAME}->[$i],
-			      $sth->{PRECISION}->[$i],
-			      $types{$sth->{TYPE}->[$i]});
-      }
+            if(!exists($types{$sth->{TYPE}->[$i]}) || $types{$sth->{TYPE}->[$i]} eq "unknown") {
+              print STDERR "Warning: Unknown database type $sth->{TYPE}->[$i] ";
+              print STDERR "used in column $sth->{NAME}->[$i]\n";
+            }
 
-      # Write the data to the formatter
-      my $rows=0;
-      while(my @data = $sth->fetchrow_array) {
-	if(exists($opts->{"max-rows"}) && $rows>=$opts->{"max-rows"}) {
-	  last;
-	}
+            $formatter->addColumn($sth->{NAME}->[$i],
+                                  $sth->{PRECISION}->[$i],
+                                  $types{$sth->{TYPE}->[$i]});
+          }
 
-	$formatter->addRow(@data);
-	$rows++;
+          # Write the data to the formatter
+          my $rows=0;
+          while(my @data = $sth->fetchrow_array) {
+            if(exists($opts->{"max-rows"}) && $rows>=$opts->{"max-rows"}) {
+              last;
+            }
 
-	# Report number of rows received if we have a lot of results
-	if(($rows%100)==0 && $tty) {
-	  my $old=$|;
-	  $|=1;
-	  print "Received $rows rows...\r";
-	  $|=$old;
-	}
-      }
-      if(defined($sth->err))
-      {
-	&raiseError($dbh,$sth,"sth->fetchrow_array",$statement);
-      }
+            $formatter->addRow(@data);
+            $rows++;
 
-      # Display the table
-      $formatter->display;
+            # Report number of rows received if we have a lot of results
+            if(($rows%100)==0 && $tty) {
+              my $old=$|;
+              $|=1;
+              print "Received $rows rows...\r";
+              $|=$old;
+            }
+          }
+          if(defined($sth->err))
+          {
+            &raiseError($dbh,$sth,"sth->fetchrow_array",$statement);
+          }
 
-      if($report && exists($opts->{count}) && $formatter->getStyleInfo("rowcount")) {
-	print "\n", $rows, " row(s) returned.\n";
-      }
+          # Display the table
+          $formatter->display;
+
+          if($report && exists($opts->{count}) && $formatter->getStyleInfo("rowcount")) {
+            print "\n", $rows, " row(s) returned.\n";
+          }
+
+          if(exists($sth->{syb_more_results}) && $sth->{syb_more_results}) {
+            print "\n";
+          }
+        }
+      } while(exists($sth->{syb_more_results}) && $sth->{syb_more_results});
     } else {
       # Non-select statement. Simply output row count
       if($report && exists($opts->{count})) {
-	print $sth->rows, " row(s) affected.\n";
+        print $sth->rows, " row(s) affected.\n";
       }
     }
 
@@ -264,7 +363,9 @@ sub main {
     $sth->finish || &raiseError($dbh,$sth,"sth->finish",$statement);
   }
 
-  $dbh->commit || &raiseError($dbh,$dbh,"dbh->commit");
+  if(!exists($opts->{"auto-commit"})) {
+    $dbh->commit || &raiseError($dbh,$dbh,"dbh->commit");
+  }
 
   $dbh->disconnect || &raiseError($dbh,$dbh,"dbh->disconnect");
 }
